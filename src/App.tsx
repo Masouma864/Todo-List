8af97f47c22a918e6783a293fab8b035931ff224
masouma <masouma2020@gmail.com>
2023-01-16 11:00:42 +0430

    Create todo-list

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
+import { Todo } from './todo.model';
+
+
+const App: React.FC = () => {
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const todoAddHandler = (text: string) => {
+    setTodos(prevTodos  => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text }
+    ]);
+  };
+  const todoDeleteHandler=(todoId:string)=>{
+setTodos(prevTodos=>{
+  return prevTodos.filter(todo=>todo.id!==todoId)
+}
+ 
+)
+  }
+  return (
+    <div className='App'>
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
+      <NewTodo onAddTodo={todoAddHandler} />
+    </div>
+  )
+}
+
+
+export default App;
